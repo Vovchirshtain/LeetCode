3e9a49e82cb3a98d10f3fecf7d5bf11cdbb98f2d
Vladimir Yakovets <Vovchirshtain@yandex.ru>
2024-05-13 22:36:20 +0300

    #9 Palindrome Number

--- LeetCode/LeetCode.cpp
+++ LeetCode/LeetCode.cpp
@@ -2,63 +2,48 @@
 #include <vector>
 #include <unordered_map>
 
-// ============================================== Two Sum ========================================================
+// ============================================== Palindrome Number ==============================================
 /*
-* Given an array of integers numsand an integer target, return indices of the two numbers such that they add up to target.
-* 
-* You may assume that each input would have exactly one solution, and you may not use the same element twice.
-* 
-* You can return the answer in any order.
+* Given an integer x, return true if x is a palindrome, and false otherwise.
 * 
 * Example 1:
-* Input: nums = [2,7,11,15], target = 9
-* Output: [0,1]
-* Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+* Input: x = 121
+* Output: true
+* Explanation: 121 reads as 121 from left to right and from right to left.
 * 
 * Example 2:
-* Input: nums = [3,2,4], target = 6
-* Output: [1,2]
+* Input: x = -121
+* Output: false
+* Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
 * 
 * Example 3:
-* Input: nums = [3,3], target = 6
-* Output: [0,1]
+* Input: x = 10
+* Output: false
+* Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
 */
 // ---------------------------------------------------------------------------------------------------------------
 class Solution
 {
 public:
-    std::vector<int> twoSum(std::vector<int> &nums, int target) 
+    bool isPalindrome(int x) 
     {
-        int diff;
-        unsigned int i;
-        std::unordered_map<int, int>::iterator it;
-        std::unordered_map<int, int> hash_t;
+        int half = 0;
 
-        for (i = 0; i < nums.size(); i++)
+        if (x < 0 || (x != 0 && x % 10 == 0) )
+            return false;
+
+        while (half < x)
         {
-            diff = target - nums[i];
-            it = hash_t.find(diff);
-
-            if (it != hash_t.end() ) return {hash_t[diff], (int)i};
-            else hash_t[nums[i]] = i;
+            half = (half * 10) + (x % 10);
+            x = x / 10;
         }
 
-        return {};
+        return (x == half) || (x == half / 10); 
     }
 };
 // ---------------------------------------------------------------------------------------------------------------
 int main()
 {
-    std::vector<int> nums = {1, 2, 3, 4};
-    int target = 3;
-
-    Solution solution;
-    std::vector<int> output = solution.twoSum(nums, target);
-
-    printf("output: ");
-    for (int digit: output)
-        printf("%i ", digit);
-    printf("\n");
     return 0;
 }
 
